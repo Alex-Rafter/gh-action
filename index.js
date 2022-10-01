9149dcbf3128a04625529711e9c718f79cfd085a
Alex Rafter <alexrafteremail@gmail.com>
2022-10-02 00:23:38 +0100

    node test with shelljs

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
+const shell = require('shelljs');
 // const core = require('@actions/core');
 // const github = require('@actions/github');
+const list = shell.ls();
 
 console.log('a simple test')
+console.log(list)
